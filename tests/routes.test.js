13d28dd116d07ad6da5f131cb556fa388deff4a6
Kelvin Gobo <gobokel@gmail.com>
2020-04-01 19:45:12 +0100

    fix async route operation

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,11 +1,13 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe('Get page', () => {
-  it('should return hello world', async (done) => {
+describe('Get route', () => {
+  it('page should return hello world', async (done) => {
     const res = await request(app).get('/')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toEqual('Hello world')
     done()
   })
 })
+
+afterAll(async () => { await app.close() })
